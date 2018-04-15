bdcab220bd9fcda5e789221759ae230a3b9d9879
Kevin Lu <kevin999990@hotmail.com>
2018-04-15 19:54:06 +0800

    Complete Register Fragment with Register to FireBase and Update Display Name

--- app/src/main/java/com/example/productfinding/LoginFragment.java
+++ app/src/main/java/com/example/productfinding/LoginFragment.java
@@ -1,20 +1,52 @@
 package com.example.productfinding;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
+    private static final String TAG = "LoginFragment";
+    private FirebaseAuth mFirebaseAuth;
 
     public LoginFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // TODO: 4/15/2018
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
+        if (currentUser != null) {
+            //user is signed in
+            /*updateUI(currentUser);*/
+
+        } else {
+            //no user is signedin
+        }
+
+
     }
 
     @Override
@@ -23,9 +55,22 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
+        TextView mEmail = view.findViewById(R.id.login_et_email);
+        TextView mPassword = view.findViewById(R.id.login_et_password);
+
         Button loginBtn = view.findViewById(R.id.login_btn_login);
-        loginBtn.setOnClickListener(v ->
-                Toast.makeText(getContext(), "Hello, This is LOGIN Button", Toast.LENGTH_SHORT).show());
+        loginBtn.setOnClickListener((View v) -> {
+            Log.d(TAG, "onCreateView: Login Button Clicked");
+
+            Toast.makeText(getContext(), "Hello, This is LOGIN Button", Toast.LENGTH_SHORT).show();
+
+
+        });
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/example/productfinding/RegisterFragment.java
+++ app/src/main/java/com/example/productfinding/RegisterFragment.java
@@ -2,37 +2,138 @@ package com.example.productfinding;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegisterFragment extends Fragment {
+    private static final String TAG = "RegisterFragment";
+    private FirebaseAuth mFirebaseAuth;
 
+    private TextView mName;
+    private TextView mEmail;
+    private TextView mPassword;
+    private View mView;
+    private Button registerBtn;
 
     public RegisterFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: Create Register Fragment");
+        super.onCreate(savedInstanceState);
+        mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: Create Register Fragment View");
+
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_register, container, false);
-
-        Button registerBtn = view.findViewById(R.id.register_btn_register);
-        registerBtn.setOnClickListener((v) -> {
-            Toast.makeText(getContext(), getString(R.string.txt_register), Toast.LENGTH_SHORT).show();
-        });
-
-        return view;
+        mView = inflater.inflate(R.layout.fragment_register, container, false);
+        init();
+        return mView;
     }
 
+    private void init() {
+        Log.d(TAG, "init: Initializing Value");
+        mName = mView.findViewById(R.id.register_et_name);
+        mEmail = mView.findViewById(R.id.register_et_email);
+        mPassword = mView.findViewById(R.id.register_et_password);
+        registerBtn = mView.findViewById(R.id.register_btn_register);
+
+        registerBtn.setOnClickListener(v -> {
+                    Log.d(TAG, "init: Register Button Clicked");
+                    registerUser();
+                }
+        );
+    }
+
+    private void registerUser() {
+        Log.d(TAG, "registerUser: Registering User");
+
+        if (!isEditTextFieldValid()) return;
+        showProgressBar(true);
+
+        String password = mPassword.getText().toString();
+        String email = mEmail.getText().toString();
+
+        mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(registerTask -> {
+                            if (registerTask.isSuccessful()) {
+                                Log.d(TAG, "registerUser: Success");
+                                Toast.makeText(getContext(), "Success Register", Toast.LENGTH_SHORT).show();
+                                updateDisplayName();
+
+                            } else {
+                                Log.e(TAG, "registerUser: Fail", registerTask.getException());
+                                Toast.makeText(getContext(), registerTask.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                            showProgressBar(false);
+                        }
+                );
+    }
+
+    private void updateDisplayName() {
+        Log.d(TAG, "updateDisplayName: Update User Display Name");
+        String name = mName.getText().toString();
+
+        mFirebaseAuth.getCurrentUser().updateProfile(new UserProfileChangeRequest.Builder().setDisplayName(name).build())
+                .addOnCompleteListener(
+                        updateTask -> {
+                            if (updateTask.isSuccessful()) {
+                                Log.d(TAG, "updateDisplayName: Success Update Display Name");
+                            } else
+                                Log.e(TAG, "updateDisplayName: Fail Update Display Name", updateTask.getException());
+                        }
+                );
+    }
+
+    private boolean isEditTextFieldValid() {
+        Log.d(TAG, "isEditTextFieldValid: Check is Edit Text Field Valid");
+        String fieldRequiredErr = getString(R.string.err_field_required);
+
+        if (mName.getText().toString().isEmpty()) {
+            mName.setError(fieldRequiredErr);
+            return false;
+        } else if (mEmail.getText().toString().isEmpty()) {
+            mEmail.setError(fieldRequiredErr);
+            return false;
+        } else if (isValidEmail(mEmail.getText().toString())) {
+            mEmail.setError(getString(R.string.err_wrong_email_format));
+            return false;
+        } else if (mPassword.getText().toString().isEmpty()) {
+            mEmail.setError(fieldRequiredErr);
+            return false;
+        } else if (mPassword.getText().toString().length() < 6) {
+            mPassword.setError(getString(R.string.err_password_length));
+            return false;
+        }
+        return true;
+    }
+
+    private void showProgressBar(Boolean show) {
+        mView.findViewById(R.id.progress_bar_layout).setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+
+    public final static boolean isValidEmail(CharSequence target) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
 }
