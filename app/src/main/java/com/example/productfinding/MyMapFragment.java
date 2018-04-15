f88ce9371cfc0c66b0c3b19b29be1f773524aefe
Kevin Lu <kevin999990@hotmail.com>
2018-04-16 03:33:39 +0800

    done setting on frame size for map

--- app/src/main/java/com/example/productfinding/MyMapFragment.java
+++ app/src/main/java/com/example/productfinding/MyMapFragment.java
@@ -1,7 +1,12 @@
 package com.example.productfinding;
 
 
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -19,13 +24,19 @@ import com.google.android.gms.maps.model.MarkerOptions;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MyMapFragment extends Fragment implements OnMapReadyCallback {
+public class MyMapFragment extends Fragment implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
     private static final String TAG = "MyMapFragment";
+    public static final int PERMISSIONS_REQUEST_LOCATION = 99;
     private GoogleMap mMap;
     private View mView;
 
     public MyMapFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -45,9 +56,54 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback {
         Log.d(TAG, "onMapReady: Map Ready");
         mMap = googleMap;
 
+        setMyCurrentLocationEnabled(true);
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private boolean checkLocationPermission() {
+        Log.d(TAG, "checkLocationPermission: Check is Location Permission Granted");
+
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                &&
+                ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(getContext())
+                        .setTitle("Location Permission")
+                        .setMessage("This Application needs Location Permission to Continue")
+                        .setPositiveButton("OK", (dialog, which) -> {
+                            requestLocationPermission();
+                        }).create().show();
+            } else
+                requestLocationPermission();
+            return false;
+        }
+        return true;
+    }
+
+    private void requestLocationPermission() {
+        Log.d(TAG, "requestLocationPermission: Requesting Location Permission");
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSIONS_REQUEST_LOCATION);
+    }
+
+    private void setMyCurrentLocationEnabled(boolean enabled) {
+        Log.d(TAG, "setMyCurrentLocationEnabled: to " + enabled);
+        if (checkLocationPermission())
+            mMap.setMyLocationEnabled(enabled);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                setMyCurrentLocationEnabled(true);
+        }
     }
 }
