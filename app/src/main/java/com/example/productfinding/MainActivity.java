7037a816a4419082591fb6ef4001660fc4f80b36
Kevin Lu <kevin999990@hotmail.com>
2018-07-31 23:07:54 +0800

    Main Activity

--- app/src/main/java/com/example/productfinding/MainActivity.java
+++ app/src/main/java/com/example/productfinding/MainActivity.java
@@ -4,19 +4,15 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.productfinding.login.LoginActivity;
@@ -24,103 +20,75 @@ import com.example.productfinding.model.User;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
     private static final String TAG = "MainActivity";
 
-    private DrawerLayout drawer;
+    private DrawerLayout mDrawerLayout;
     private Toolbar toolbar;
+    private ActionBar actionBar;
     private User loginUser;
     private NavigationView navigationView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nav);
+        setContentView(R.layout.activity_main);
 
-        initializeVariable();
-        isUserExist();
-        initNavigationDrawerHeader();
-    }
-
-
-    private void initializeVariable() {
-        Log.d(TAG, "initializeVariable: Initialize Variable");
-        loginUser = (User) getIntent().getSerializableExtra("loginUser");
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                getParent(), drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
-//        navigationView.getMenu().getItem(1).setChecked(true);
-        onNavigationItemSelected(navigationView.getMenu().getItem(1));
-    }
 
-    private boolean isUserExist() {
-        Log.d(TAG, "isUserExist: Check is User Exist");
-        if (loginUser != null) {
-//            Toast.makeText(getApplicationContext(), "You Login As: " + loginUser.getName(), Toast.LENGTH_SHORT).show();
-            return true;
-        } else {
-            Toast.makeText(getApplicationContext(), "Null", Toast.LENGTH_SHORT).show();
-            return false;
-        }
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Log.d(TAG, "onNavigationItemSelected: Select Nav Item");
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        item.setChecked(true);
+        mDrawerLayout.closeDrawers();
+
         switch (item.getItemId()) {
+            case R.id.nav_history:
+                Toast.makeText(this, "History Selected", Toast.LENGTH_SHORT).show();
+                return true;
             case R.id.nav_logout:
                 signOut();
-                break;
-            case R.id.nav_profile:
-                Toast.makeText(getApplicationContext(), "Profile clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.nav_map:
-
-                fragmentManager.beginTransaction().replace(R.id.main_container, new MyMapFragment()).commit();
-//                Toast.makeText(getApplicationContext(), "Map clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.nav_history:
-                Toast.makeText(getApplicationContext(), "History clicked", Toast.LENGTH_SHORT).show();
-                break;
+                return true;
             case R.id.nav_setting:
-                Toast.makeText(getApplicationContext(), "Setting clicked", Toast.LENGTH_SHORT).show();
-                break;
+                Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.nav_map:
+                Toast.makeText(this, "Map", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.nav_profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                return true;
+
         }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return false;
+        return true;
     }
 
+    /*
+     * Open the drawer when the button is tapped
+     * To open the drawer when the user taps on the nav drawer button,
+     * override onOptionsItemSelected() to hook into the options menu framework and listen for when the user taps the item with the ID android.R.id.home.
+     * Then call openDrawer() to open the nav drawer:
+     */
     @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
-    }
-
-    private void initNavigationDrawerHeader() {
-        Log.d(TAG, "initNavigationDrawer: Initialize Navigation Drawer Header");
-
-        View headerView = navigationView.inflateHeaderView(R.layout.nav_header_nav);
-
-        TextView userDisplayName = headerView.findViewById(R.id.nav_header_tv_name);
-        String _name = loginUser.getName();
-        userDisplayName.setText(_name);
-
-        TextView userEmail = headerView.findViewById(R.id.nav_header_tv_email);
-        String _email = loginUser.getEmail();
-        userEmail.setText(_email);
+        return super.onOptionsItemSelected(item);
     }
 
     private void signOut() {
@@ -139,4 +107,6 @@ public class MainActivity extends AppCompatActivity
         builder.create();
         builder.show();
     }
+
+
 }
